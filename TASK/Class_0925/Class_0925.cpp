066ca8ab2d7c11c08215d837d113a3686efb4c1d
seo eunjeong <fxclimber@gmail.com>
2024-09-25 14:00:04 +0900

    복습하고 올리기

---
+++ TASK/Class_0925/Class_0925.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+
+
+class Player
+{
+public:
+    Player();
+    ~Player();
+
+    void CreateWeapon(){}
+    void Attack(){}
+    void Die(){}
+
+    int& GetHp() { return Hp; }
+    int& GetAtt() { return Att; }
+
+public:
+    const char* name = "Knight";//8
+
+    int Def = 7;//4
+    int Att = 8;//4
+    short check = 30;//2
+    bool win = false;//1
+    int level = 0;//4
+    __int64 exp = 0;//8
+    int Hp2 = 200;
+
+
+private:
+    int Hp = 100;//4
+
+};
+
+
+
+int main()
+{
+    //객체 생성
+    //Player newPlayer1 = Player();
+    //Player newPlayer2 = Player();
+
+    Player newPlayer1;
+    Player newPlayer2;
+    
+    //객체 크기
+    int sizeFunc = sizeof(Player);
+
+    //함수참조 변수
+    int& getHp = newPlayer1.GetHp();
+    int& hp2 = newPlayer1.Hp2;
+    //강제 형변환한 주소값
+    __int64 Add1 = reinterpret_cast<__int64>(&getHp);
+    __int64 Add2 = reinterpret_cast<__int64>(&hp2);
+
+
+
+    int a = 0;
+
+    return  0;
+}
+
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
