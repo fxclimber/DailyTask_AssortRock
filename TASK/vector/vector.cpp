9ab20d80430802fb816e5cb24240c9bcbd0ba380
seo eunjeong <fxclimber@gmail.com>
2024-10-22 11:22:46 +0900

    1022

--- TASK/vector/vector.cpp
+++ TASK/vector/vector.cpp
@@ -10,33 +10,35 @@ int main()
 	{
 	//------------------------std::vector---------------------
 
-	////스택에 객체 직접 생성
-	//std::vector<int> v;
-	////힙에 동적 메모리 할당시, 포인터로 선언해야한다 
-	//std::vector<int>* v1 = new std::vector<int>;
+	//스택에 객체 직접 생성
+	std::vector<int> v;
+	//힙에 동적 메모리 할당시, 포인터로 선언해야한다 
+	std::vector<int>* v1 = new std::vector<int>;
 
-	//v.reserve(100);
-	//std::cout << "v.capacity()" << v.capacity() << std::endl;
-
-
-	//for (int i = 0; i < 100; ++i)
-	//{
-	//	v.push_back(i);
-	//	std::cout << v[i] << std::endl;
-	//}
-	////std::cout << "sizeof" << sizeof(v) << std::endl;//틀림, size()맞음
-	//std::cout << "v.capacity()" << v.capacity() << std::endl;
-	//std::cout << "size" << v.size() << std::endl;
-
-	//std::vector<int> v3 = v;
+	v.reserve(100);
+	std::cout << "v.capacity()" << v.capacity() << std::endl;
 
 
+	for (int i = 0; i < 100; ++i)
+	{
+		v.push_back(i);
+		std::cout << v[i] << std::endl;
+	}
+	//std::cout << "sizeof" << sizeof(v) << std::endl;//틀림, size()맞음
+	std::cout << "v.capacity()" << v.capacity() << std::endl;
+	std::cout << "size" << v.size() << std::endl;
 
-	//v.clear();	
-	//std::cout << "v.capacity()" << v.capacity() << std::endl;
-	//std::cout << "v.size()" << v.size() << std::endl;
+	std::vector<int> v3 = v;
+	v3 = v;
+
+
+
+	v.clear();	
+	std::cout << "v.capacity()" << v.capacity() << std::endl;
+	std::cout << "v.size()" << v.size() << std::endl;
 
 	}
+
 
 
 
@@ -55,7 +57,11 @@ int main()
 
 	std::cout << "-------------end-----------------" << std::endl;
 
-	UVector<int> uv2 = uv;//내 벡터를 잘못만들었나보다. 벡터카피하니까 프로그램이 문제있는지 콘솔창이 안닫힌다.
+	UVector<int> uv2 = uv;
+
+	UVector<int> uv3;
+	uv3 = uv;
+
 	for (int i = 0; i < uv2.getCapacity(); i++)
 	{
 		std::cout << "uv2 ("<<i<<") : " << uv2[i] << std::endl;
